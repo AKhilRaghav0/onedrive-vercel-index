7bbb896cbf9c97492eca909f579daf6a75682cc8
Sam <75714657+pichuiscute@users.noreply.github.com>
2023-01-19 13:02:20 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🥸 Pikachus-sus-data','/🎧 Flac Music','/🎥 Movies'],
+  protectedRoutes: ['/🥸 Pikachus-sus-data','/🎥 Movies'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:parasiticvenom@gmail.com',
